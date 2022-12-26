36b562a6c7e0345b142905799f0a00833298a19d
dazai404 <woysen@gmail.com>
2022-12-27 00:59:57 +0300

    refactor

--- api/models/user.go
+++ api/models/user.go
@@ -1,15 +1,15 @@
 package models
 
 type TgUser struct {
-	Id                      *string `json:"id"`
-	IsBot                   bool    `json:"is_bot"`
-	FirstName               string  `json:"first_name"`
-	LastName                string  `json:"last_name"`
-	Username                string  `json:"username"`
-	LanguageCode            string  `json:"language_code"`
-	IsPremium               bool    `json:"is_premium"`
-	AddedToAttachmentMenu   bool    `json:"added_to_attachment_menu"`
-	CanJoinGroups           bool    `json:"can_join_groups"`
-	CanReadAllGroupMessages bool    `json:"can_read_all_group_messages"`
-	SupportsInlineQueries   bool    `json:"supports_inline_queries"`
+	UserId                  *int   `json:"id" bson:"_id"`
+	IsBot                   bool   `json:"is_bot" bson:"is_bot"`
+	FirstName               string `json:"first_name" bson:"first_name"`
+	LastName                string `json:"last_name" bson:"last_name"`
+	Username                string `json:"username" bson:"username"`
+	LanguageCode            string `json:"language_code" bson:"language_code"`
+	IsPremium               bool   `json:"is_premium" bson:"is_premium"`
+	AddedToAttachmentMenu   bool   `json:"added_to_attachment_menu" bson:"added_to_attachment_menu"`
+	CanJoinGroups           bool   `json:"can_join_groups" bson:"can_join_groups"`
+	CanReadAllGroupMessages bool   `json:"can_read_all_group_messages" bson:"can_read_all_group_messages"`
+	SupportsInlineQueries   bool   `json:"supports_inline_queries" bson:"supports_inline_queries"`
 }
